302b738e9f13b8c8de08aabee52a8ff16f9c1ba2
Paulo Henrique <paulo.h.mendes25@gmail.com>
2021-09-07 01:51:21 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/ValeteDigital" target="_blank" rel="noreferrer">Paulo Mendes</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Aplicação desenvolvida para Tigrão Distribuidora de Enxovais</small></p>
+                </div>
+            </footer></div>
+    );
+}
+
+export default Footer;
