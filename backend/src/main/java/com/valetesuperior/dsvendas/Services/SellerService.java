bdba27a5966e770a0fd210211e83079d2ea61d97
Paulo Henrique <paulo.h.mendes25@gmail.com>
2021-09-09 00:02:28 -0300

    Layers

---
+++ backend/src/main/java/com/valetesuperior/dsvendas/Services/SellerService.java
@@ -0,0 +1,23 @@
+package com.valetesuperior.dsvendas.Services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valetesuperior.dsvendas.dto.SellerDTO;
+import com.valetesuperior.dsvendas.entities.Seller;
+import com.valetesuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
