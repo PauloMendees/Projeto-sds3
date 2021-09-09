b95150ecd4ad0198a1de51b6935ad775678994b0
Paulo Henrique <paulo.h.mendes25@gmail.com>
2021-09-09 02:56:39 -0300

    Group by search

--- backend/src/main/java/com/valetesuperior/dsvendas/Services/SaleService.java
+++ backend/src/main/java/com/valetesuperior/dsvendas/Services/SaleService.java
@@ -2,6 +2,8 @@ package com.valetesuperior.dsvendas.Services;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.valetesuperior.dsvendas.dto.SaleDTO;
+import com.valetesuperior.dsvendas.dto.SaleSuccessDTO;
+import com.valetesuperior.dsvendas.dto.SaleSumDTO;
 import com.valetesuperior.dsvendas.entities.Sale;
 import com.valetesuperior.dsvendas.repositories.SaleRepository;
 import com.valetesuperior.dsvendas.repositories.SellerRepository;
@@ -27,5 +31,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
--- backend/src/main/java/com/valetesuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/valetesuperior/dsvendas/controllers/SaleController.java
@@ -1,6 +1,6 @@
 package com.valetesuperior.dsvendas.controllers;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.valetesuperior.dsvendas.Services.SaleService;
 import com.valetesuperior.dsvendas.dto.SaleDTO;
+import com.valetesuperior.dsvendas.dto.SaleSuccessDTO;
+import com.valetesuperior.dsvendas.dto.SaleSumDTO;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -23,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/sum-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
